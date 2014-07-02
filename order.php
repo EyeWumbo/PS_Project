d50c352751a17b89c97b66e1835ef7aa70ed3a39
wuw94 <wesley@Wesleys-MacBook-Air.local>
2014-07-02 13:07:52 -0700

    backend yay

---
+++ order.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html class="" lang="en">
+
+  <head>
+ 
+    <title>Doublr</title>
+
+    <link rel="icon" type="image/png" href="">
+
+    <link rel="icon" 
+      type="image/png" 
+      href="wes/Favicon.png">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- CSS -->
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/icons.css" rel="stylesheet">
+    <link href="css/jquery.bxslider.css" rel="stylesheet">
+    <link href="css/supersized.css" rel="stylesheet">
+    <link href="css/effect.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive-body.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="css/metro-style.css" rel="stylesheet">
+    <link href="css/Met-Js.css" rel="stylesheet">
+
+    <!-- Js -->
+
+    <script src="js/jquery.min.js"></script>
+
+
+    <!-- Stripe Payment Script -->
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+    <script type="text/javascript">// <![CDATA[Stripe.setPublishableKey('lksdajSDFmn2345nv');// ]]></script>
+    <!-- /Stripe Payment Script -->
+
+
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-52426101-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <!-- /Google Analytics -->
+
+    <?php
+    echo '<p>Hello World</p>';
+    ?>
+
+  </head>
+
+
+
+
+  <body class="onepage">
+    <div id="preloader"></div>
+    <a id="top"></a>
+
+
+
+    <!-- NAV -->
+    <nav id="nav-desktop" class="nav-normal"> 
+      <div class="container">
+        <h1 id="logo"><a class="scroll" href="index.html"><img class="logo" src="img/logo.png" alt=""></a></h1>
+        <ul>
+          <li><a href="#learn"><span>Learn More</span></a></li>
+          <li><a href="#order"><span>Order</span></a></li>
+          <li><a href="#contact"><span>Contact</span></a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- /NAV -->
+
+
+
+    <!-- What Do We Do? -->
+    <section id="home" class="page">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="title clearfix pad-top">
+            <h1></h1>
+            <h1 data-animated="bounceIn">What Do We Do?</h1>
+            <img src="img/lini.png" alt="" data-animated="bounceIn">
+          </div>
+          <h1 class="under-l" data-animated="bounceIn">We provide a way of making the best memories last even longer with lifelike replicas.</h1>
+        </div>
+      </div>
+      <div class="row ">
+        <div id="about-wrap" class="pad-top">
+          <div id="about-desc" class="col-sm-6 col-md-6 pad-top pad-bottom">
+            <a style="margin-left: 10%" href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=33.697945,-117.846761&zoom=16&size=600x300&maptype=roadmap
+&markers=color:red%7Clabel:PeopleSpace%7C33.697945, -117.846761" alt="" data-animated="bounceIn"></a>
+          </div>
+          <div class="col-md-6 col-sm-6 pad-top">
+            <div class="service-icon" data-animated="fadeInRight">
+              <span class="icon-gear"></span>
+              <h3>Tangible</h3>
+              <p>Using the latest 3D printing technology</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6 pad-hbottom">
+            <div class="service-icon" data-animated="fadeInRight">
+              <span class="icon-mobile"></span>
+              <h3>Lifelike</h3>
+              <p>High quality figurines that look alive</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="service-icon" data-animated="fadeInRight">
+              <span class="icon-book"></span>
+              <h3>Quick</h3>
+              <p>Scan yourself in 60 seconds</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /What Do We Do? -->
+
+
+
+    <!-- We Sell Locally -->
+    <section id="home" class="page">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="title clearfix pad-top">
+            <h1></h1>
+            <h1 data-animated="bounceIn">We're Local!</h1>
+            <img src="img/lini.png" alt="" data-animated="bounceIn">
+          </div>
+          <h1 class="under-l" data-animated="bounceIn">Visit us at our humble headquarters: 1691 Kettering Street, Irvine, CA</h1>
+        </div>
+      </div>
+      <div class="row ">
+        <div id="about-wrap" class="pad-top">
+          <div id="about-desc" class="col-md-12">
+            <a style="margin-left: 10%" href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=33.697945,-117.846761&zoom=16&size=600x300&maptype=roadmap
+&markers=color:red%7Clabel:PeopleSpace%7C33.697945, -117.846761" alt="" data-animated="bounceIn"></a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /What Do We Do? -->
+
+    <!-- JS -->
+
+    <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+    <script type="text/javascript" src="js/jquery.sticky.js"></script>
+    <script type="text/javascript" src="js/jquery.parallax.js"></script>
+    <script type="text/javascript" src="js/jquery.easypiechart.min.js"></script>
+    <script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+    <script type="text/javascript" src="js/jquery.timelinr-0.9.53.js"></script>
+    <script type="text/javascript" src="js/jquery.appear.js"></script>
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="js/retina.min.js"></script>
+    <script type="text/javascript" src="js/supersized.3.2.7.min.js"></script>
+    <script type="text/javascript" src="js/supersized.shutter.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script type="text/javascript" src="js/MetroJs.js"></script>
+    <script type="text/javascript" src="js/custom.metro.js"></script>
+    <script src="js/jquery.superslides.min.js"></script>
+    <script src="js/jquery.sudoslider.min.js"></script>
+    <script src="js/service-js.js"></script>
+
+
+  </body>
+</html>
